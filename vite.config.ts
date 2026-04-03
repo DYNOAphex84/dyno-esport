b9aa04c04eeaa5c1970bff27fbb184de78417af2
DYNOAphex84 <thibaut.llorens13090@gmail.com>
2026-04-03 07:37:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/dyno-esport/',
+  root: 'source',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     sourcemap: false
   }
 })
