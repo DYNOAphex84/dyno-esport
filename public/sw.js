80974379029b206142c7a97865319ba9fe864506
DYNOAphex84 <thibaut.llorens13090@gmail.com>
2026-04-25 06:12:24 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,41 @@
-const CACHE_NAME = 'dyno-esport-v1'
+const CACHE_NAME = 'dyno-esport-v1';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/dyno-esport/',
-        '/dyno-esport/index.html'
-      ])
+      return cache.addAll(['/']);
     })
-  )
-})
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
     })
-  )
-})
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
