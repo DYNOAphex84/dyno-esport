8c94dad532363f60abbdeb1c9b24ad7a1dfcfc19
DYNOAphex84 <thibaut.llorens13090@gmail.com>
2026-04-25 06:14:41 +0200

    Update main.tsx

--- source/main.tsx
+++ source/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('SW registered:', reg))
-      .catch(err => console.log('SW error:', err))
+      .then(reg => console.log('✅ SW registered:', reg))
+      .catch(err => console.log('❌ SW error:', err))
   })
 }
